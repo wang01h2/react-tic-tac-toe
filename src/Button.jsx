c78af0c4fceb3c66d030a2aed807210d0d7bd1b8
wang01h2 <1299839398@qq.com>
2022-08-26 10:27:09 +0800

    feat(App.css      App.jsx      Button.jsx      index.css): 根据新文档学习
    根据新文档学习

---
+++ src/Button.jsx
@@ -0,0 +1,7 @@
+function Button() {
+    return (
+        <button className="bg-gray-100 p-2 px-4 rounded-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:bg-gray-100 focus:ring-opacity-50">I'm a button</button>
+    )
+}
+
+export default Button
