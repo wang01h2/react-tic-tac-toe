31c55aa3dca85b67f880b47c98d97f92df0a6c44
wang01h2 <1299839398@qq.com>
2022-08-29 15:49:15 +0800

    feat(App.jsx      Button.jsx): 🆕根据新文档学习,从class写法升级到更推荐的hooks写法～
    🆕根据新文档学习,从class写法升级到更推荐的hooks写法～

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
 import Button from "./Button.jsx";
-import {Component} from "react";
 
-class App extends Component{
-    render() {
-        return (
-            <div>
-                <Button onClick={() => console.log('click done.')}>I'm a button</Button>
-            </div>
-        )
-    }
+function App(){
+    return (
+        <div>
+            <Button onClick={() => console.log('click done.')}>I'm a button</Button>
+        </div>
+    )
 }
 
 export default App
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,20 +1,13 @@
-import {Component} from "react";
-
-class Button extends Component {
-    constructor(props) {
-        super(props);
+export function Button(props) {
+    const handleClick = () => {
+        if(!props.onClick) return
+        props.onClick()
     }
-    handleClick = () => {
-        if(!this.props.onClick) return
-        this.props.onClick()
-    }
-    render() {
-        return (
-            <button
-                className="bg-gray-100 p-2 px-4 rounded-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:bg-gray-100 focus:ring-opacity-50"
-                onClick={this.handleClick}>{this.props.children}</button>
-        )
-    }
+     return (
+         <button
+             className="bg-gray-100 p-2 px-4 rounded-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:bg-gray-100 focus:ring-opacity-50"
+             onClick={handleClick}>{props.children}</button>
+     )
 }
 
 export default Button
