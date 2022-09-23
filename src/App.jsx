845d5ce24d01231ab060b2ac242ac28c4e58084e
wang01h2 <1299839398@qq.com>
2022-09-23 15:56:07 +0800

    fix(App.jsx      Button.jsx): 错误的编写
    错误的编写

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 import Button from "./Button.jsx";
 
+
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+]
+
+const listItem = products.map(product => {
+    return <li key={product.id} style={{color: product.isFruit ? 'green' : 'blue'}}>
+        {product.title}</li>
+})
+
 function App(){
     return (
         <>
             <Button type="test" onClick={() => console.log('click done.')}>I'm a button</Button>
+            <ul>{listItem}</ul>
         </>
     )
 }
