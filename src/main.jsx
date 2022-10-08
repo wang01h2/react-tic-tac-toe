15ad1015a2be278aea30f0b9e12024e2f032a7c7
wang01h2 <1299839398@qq.com>
2022-10-08 17:25:24 +0800

    fix(main.jsx): 优化代码
    优化代码

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client' // 将虚拟dom渲染到文档中变成真实dom
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Root = ReactDOM.createRoot(document.querySelector('#root'))
+
+Root.render(
+    // 严格模式只在开发环境生效 不会渲染可见UI
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
